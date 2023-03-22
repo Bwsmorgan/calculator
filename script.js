a2f9dc05957c9ae74a4866a745017891e7e5ec99
Brendan Morgan <bmo_32@hotmail.com>
2023-03-22 17:08:32 -0400

    Create the functions that populate the display when you click the number buttons and a variable to store the current number that was selected

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ function multiply(a,b) {
 function divide(a,b) {
     return a/b;
 }
+
+//Variable for 1st number selected by the user
+let usersFirstInput 
+//Variable for the operand selected by the user
+let operandSelected 
+//Variable for 2nd number selected by the user
+let usersSecondInput
+
+function operate(operator, num1, num2){
+
+    if (operator === 'add') {
+        add(num1,num2)
+    }
+    else if (operator === 'subtract') {
+        subtract(num1,num2)
+    }
+    else if (operator === 'multiply') {
+        multiply(num1,num2)
+    }
+    else if (operator === 'divide') {
+        divide(num1,num2)
+    }
+}
+
+const number = document.querySelector('.digits');
+const display = document.querySelector('#display')
+let numberSelected = 0;
+
+number.addEventListener('click', (e)=>{
+    let digitID = e.target.id;
+    var selectedValue = document.getElementById(`${digitID}`)
+    display.textContent = selectedValue.innerHTML;
+    //this is where we will store the number that was clicked
+    numberSelected = digitID;
+    console.log(digitID);
+
+})
