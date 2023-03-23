a30bf6a2adf47b7b061c1f627d75b9f38d9a4db9
Brendan Morgan <bmo_32@hotmail.com>
2023-03-22 21:40:36 -0400

    Connect event listeners to all buttons

--- script.js
+++ script.js
@@ -1,52 +1,129 @@
 function add(a,b) {
-    return a+b;
+    console.log(a)
+    console.log(a)
+
+    let sum = a+b;
+    console.log(sum);
+    
+    display.textContent = sum;
+    operandClicked = false;
 }
 
 function subtract(a,b) {
-    return a-b;
+
+    let difference = a-b;
+    display.textContent = difference;
+    operandClicked = false;
 }
 
 function multiply(a,b) {
-    return a*b;
+    
+    let product = a*b;
+    display.textContent = product;
+    operandClicked = false;
 }
 
 function divide(a,b) {
-    return a/b;
+
+    let quotient = a/b;
+    display.textContent = quotient;
+    operandClicked = false;
 }
 
 //Variable for 1st number selected by the user
-let usersFirstInput 
+let usersFirstInput = '';
 //Variable for the operand selected by the user
-let operandSelected 
+let operandSelected = '';
 //Variable for 2nd number selected by the user
-let usersSecondInput
+let usersSecondInput = '';
 
 function operate(operator, num1, num2){
 
+    let number1 = parseInt(num1);
+    let number2 = parseInt(num2);
+
+
     if (operator === 'add') {
-        add(num1,num2)
+        console.log('yes')
+        add(number1,number2)
     }
     else if (operator === 'subtract') {
-        subtract(num1,num2)
+        console.log('yes')
+        subtract(number1,number2)
     }
     else if (operator === 'multiply') {
-        multiply(num1,num2)
+        console.log('yes')
+        multiply(number1,number2)
     }
     else if (operator === 'divide') {
-        divide(num1,num2)
+        console.log('yes')
+        divide(number1,number2)
     }
 }
 
 const number = document.querySelector('.digits');
-const display = document.querySelector('#display')
-let numberSelected = 0;
+const display = document.querySelector('#display');
+const operand = document.querySelector('.operands');
+const calculate = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
+
+
 
 number.addEventListener('click', (e)=>{
-    let digitID = e.target.id;
-    var selectedValue = document.getElementById(`${digitID}`)
-    display.textContent = selectedValue.innerHTML;
-    //this is where we will store the number that was clicked
-    numberSelected = digitID;
-    console.log(digitID);
+
+    if(operandClicked === false){
+       
+        let digitID = e.target.id;
+        var selectedValue = document.getElementById(`${digitID}`);
+        display.textContent += `${selectedValue.innerHTML}`;
+        //this is where we will store the number that was clicked
+        usersFirstInput += selectedValue.innerHTML;
+        console.log(usersFirstInput);
+        console.log(digitID);
+    }
+
+    else {
+        
+        let digitID = e.target.id;
+        var selectedValue = document.getElementById(`${digitID}`);
+        display.textContent += `${selectedValue.innerHTML}`;
+        //this is where we will store the number that was clicked
+        usersSecondInput += selectedValue.innerHTML;
+        console.log(usersSecondInput);
+        console.log(digitID);
+    }    
+})
+
+
+let operandClicked = false;
+
+operand.addEventListener('click', (e)=>{
+
+    operandClicked = true;
+    console.log("operand active")
+
+    let operandID = e.target.id;
+    var selectedOperand = document.getElementById(`${operandID}`);
+    display.textContent = selectedOperand.innerHTML;
+    operandSelected = operandID;
+    console.log(operandSelected);
+
+})
+
+calculate.addEventListener('click', (e)=>{
+
+    if(usersFirstInput != '' && operandSelected != '' && usersSecondInput != ''){
+        console.log(true)
+        operate(operandSelected, usersFirstInput, usersSecondInput);
+    }
+
+})
+
+clear.addEventListener('click', ()=>{
+
+    display.textContent = '';
+    usersFirstInput = '';
+    operandSelected = '';
+    usersSecondInput = '';
 
 })
