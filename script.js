bc0d99ddecfb9b5f720c5fa7b9c0ebbcb93696de
Brendan Morgan <bmo_32@hotmail.com>
2023-04-01 20:26:09 -0400

    Store users first and second numbers and operand in indiviual arrays to access numbers based on their index when working with math operations that include more than one operand and two numbers

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function add(a,b) {
-    console.log(a)
-    console.log(a)
-
-    let sum = a+b;
-    console.log(sum);
     
+    let sum = a+b;   
     display.textContent = sum;
     operandClicked = false;
+    usersFirstInput = [''];
+    operandSelected = [''];
+    usersSecondInput = [''];
+    return sum; 
+    
+
 }
 
 function subtract(a,b) {
@@ -14,6 +16,10 @@ function subtract(a,b) {
     let difference = a-b;
     display.textContent = difference;
     operandClicked = false;
+    usersFirstInput = '';
+    operandSelected = '';
+    usersSecondInput = '';
+    return difference;
 }
 
 function multiply(a,b) {
@@ -21,6 +27,10 @@ function multiply(a,b) {
     let product = a*b;
     display.textContent = product;
     operandClicked = false;
+    usersFirstInput = '';
+    operandSelected = '';
+    usersSecondInput = '';
+    return product;
 }
 
 function divide(a,b) {
@@ -28,24 +38,27 @@ function divide(a,b) {
     let quotient = a/b;
     display.textContent = quotient;
     operandClicked = false;
+    usersFirstInput = '';
+    operandSelected = '';
+    usersSecondInput = '';
+    return quotient;
 }
 
 //Variable for 1st number selected by the user
-let usersFirstInput = '';
+let usersFirstInput = [''];
 //Variable for the operand selected by the user
-let operandSelected = '';
+let operandSelected = [];
 //Variable for 2nd number selected by the user
-let usersSecondInput = '';
+let usersSecondInput = [''];
 
 function operate(operator, num1, num2){
 
-    let number1 = parseInt(num1);
-    let number2 = parseInt(num2);
+    let number1 = parseInt(num1[0]);
+    let number2 = parseInt(num2[0]);
 
 
     if (operator === 'add') {
-        console.log('yes')
-        add(number1,number2)
+        return add(number1,number2)
     }
     else if (operator === 'subtract') {
         console.log('yes')
@@ -68,62 +81,115 @@ const calculate = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
 
 
+const clicked = 0;
+let equalWasClicked = false;
+let operandClicked = false;
+
 
 number.addEventListener('click', (e)=>{
+    
+    if(equalWasClicked === true){
+        display.textContent = '';
+        equalWasClicked = false
 
-    if(operandClicked === false){
-       
-        let digitID = e.target.id;
-        var selectedValue = document.getElementById(`${digitID}`);
-        display.textContent += `${selectedValue.innerHTML}`;
-        //this is where we will store the number that was clicked
-        usersFirstInput += selectedValue.innerHTML;
-        console.log(usersFirstInput);
-        console.log(digitID);
     }
-
-    else {
-        
-        let digitID = e.target.id;
-        var selectedValue = document.getElementById(`${digitID}`);
+    
+    //this means a number was just clicked operand has not been clicked
+    else if(operandClicked === false){
+       
+        //let digitID = e.target.id;
+        var selectedValue = document.getElementById(`${e.target.id}`);
+    
+        display.textContent = usersFirstInput;
         display.textContent += `${selectedValue.innerHTML}`;
         //this is where we will store the number that was clicked
-        usersSecondInput += selectedValue.innerHTML;
+
+        usersFirstInput[0] = usersFirstInput[0] + selectedValue.innerHTML
+        //usersFirstInput.push(selectedValue.innerHTML);
+
+        console.log(`firstInput: ${usersFirstInput}`);
+        console.log(usersFirstInput);
+    }
+    
+    //operand has been clicked
+    else {
+
+        let digitID = e.target.id;
+        var selectedValue = document.getElementById(`${digitID}`);
+        display.textContent = usersSecondInput;
+        display.textContent += `${selectedValue.innerHTML}`;
+        //this is where we will store the number that was clicked
+        usersSecondInput[0] = usersSecondInput[0] + selectedValue.innerHTML
+
+
+        console.log(`secondInput: ${usersSecondInput}`);
         console.log(usersSecondInput);
-        console.log(digitID);
     }    
 })
 
 
-let operandClicked = false;
+
 
 operand.addEventListener('click', (e)=>{
 
-    operandClicked = true;
-    console.log("operand active")
+    //There is no reason to store an operand if the first value has not been selected by the user
+    if(usersFirstInput === []){
+        operandSelected = [];
+        operandClicked = false;
+    }
 
-    let operandID = e.target.id;
-    var selectedOperand = document.getElementById(`${operandID}`);
-    display.textContent = selectedOperand.innerHTML;
-    operandSelected = operandID;
-    console.log(operandSelected);
+    //this means the operand has been selected and first equation has been already set its assumed the second input has been selected
+    else if(operandClicked === true){
+
+        let newValue = operate(operandSelected[0], usersFirstInput[0], usersSecondInput[0]);
+        //remove 1st value stored in usersFirstInput then add new value
+        usersFirstInput[0]= newValue;
+        //usersFirstInput.push(newValue);
+        
+        console.log(usersFirstInput)
+
+        let operandID = e.target.id;
+        var selectedOperand = document.getElementById(`${operandID}`);
+        display.textContent = newValue;
+        operandSelected = operandID;
+        console.log(operandSelected);
+        operandClicked = true;
+    }
+
+
+    else{
+        operandClicked = true;
+        
+
+        let operandID = e.target.id;
+        var selectedOperand = document.getElementById(`${operandID}`);
+        display.textContent = selectedOperand.innerHTML;
+        operandSelected.push(operandID);
+        console.log(operandSelected);
+    }
+    
 
 })
+
+
 
 calculate.addEventListener('click', (e)=>{
 
-    if(usersFirstInput != '' && operandSelected != '' && usersSecondInput != ''){
+    equalWasClicked = true;
+
+    if(usersFirstInput != [] && operandSelected != [] && usersSecondInput != []){
         console.log(true)
         operate(operandSelected, usersFirstInput, usersSecondInput);
     }
-
 })
+
+
 
 clear.addEventListener('click', ()=>{
 
-    display.textContent = '';
-    usersFirstInput = '';
-    operandSelected = '';
-    usersSecondInput = '';
+    display.textContent = [];
+    usersFirstInput = [];
+    operandSelected = [];
+    usersSecondInput = [];
 
 })
